845c26e07cfa6e239f2727837a32eb0296a84646
Sireen Yasser Albalkhi <albalkhisyreen@gmail.com>
2020-12-21 12:34:08 +0300

    changed code

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string charectername = "zac"; 
+            string superpower1 = "steel armor";
+            string superpower2 = "night vision";
+
+            int age = 18;
+            float speed = 22.55f;
+
+            Console.WriteLine ("Hello my name is " + charectername + " and my age is " + age + ". I have super powers : " + superpower1 + " and " + superpower2 + ". My speed is {0}",speed);
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
